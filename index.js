a4d4ed0eb7b3d4c1bd41415b369fa4d117db25f9
Gaurav Dhawan <gaurav.d6@gmail.com>
2017-06-14 15:14:19 +0200

    Create index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function queryDatabase(){
 				console.error(err); response.send('Error ' + err);
 			} else {
 				console.log(rowCount + ' row(s) returned');
-				response.render('pages/thanks', {results: rows} ); 
+				response.render('pages/thanks', {results: results.rows} ); 
 			}     
         }
     );
