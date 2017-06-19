d119eb38517376d2d322509e0344385ec6b947b7
Gaurav Dhawan <gaurav.d6@gmail.com>
2017-06-19 13:00:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ console.log('Reading rows from the Table...');
 			} else {
 				console.log(rowCount + ' row(s) returned');
 				request.on('done',function(rowCount, more, rows){
-        			console.log(rows+'is returned'); // not empty
+        			//console.log(rows+'is returned'); // not empty
 				//response.render('pages/thanks.ejs', {results: rows} );
 				});
 			}     
