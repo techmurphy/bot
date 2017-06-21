cd0c46c91c8aedbf60ca2c637a01c9ccaaa6431e
Gaurav Dhawan <gaurav.d6@gmail.com>
2017-06-21 15:35:41 +0200

    Update mission.js

--- routes/mission.js
+++ routes/mission.js
@@ -3,13 +3,13 @@ var TYPES = require('tedious').TYPES;
 console.log('Inside the mission js file');
 
 
-router.post('/mission',function (req, res) {
+router.post('/',function (req, res) {
     console.log('about to execute the insert'+req.body);
     req.query("exec insertmission @mission")
         .param('mission', req.body, TYPES.NVarChar)
         .exec(res);
 });
-router.get('/mission',function (req, res) {
+router.get('/',function (req, res) {
     req.query("select * from mission for json path")
         .into(res, '[]');
 });
