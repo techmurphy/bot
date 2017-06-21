849c3946a114c76e0847018b662a27e18cb26bfb
Gaurav Dhawan <gaurav.d6@gmail.com>
2017-06-21 09:33:13 +0200

    Update mission.js

--- routes/mission.js
+++ routes/mission.js
@@ -1,19 +1,19 @@
 var router = require('express').Router();
 var TYPES = require('tedious').TYPES;
 
-/* GET task listing. */
+/* GET mission listing. */
 router.get('/', function (req, res) {
 
-    req.query("select * from thanks for json path")
+    req.query("select * from mission for json path")
         .into(res, '[]');
 
 });
 
-/* POST create task. */
+/* POST insert mission */
 router.post('/', function (req, res) {
     
-    req.query("exec InsertThanks @thanks")
-        .param('thanks', req.body, TYPES.NVarChar)
+    req.query("exec insertmissions @mission")
+        .param('mission', req.body, TYPES.NVarChar)
         .exec(res);
 
 });
