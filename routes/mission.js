8415a215946e04f2064be57237b075adae031ffb
Gaurav Dhawan <gaurav.d6@gmail.com>
2017-06-23 14:48:14 +0200

    Update mission.js

--- routes/mission.js
+++ routes/mission.js
@@ -13,6 +13,7 @@ router.post('/',function (req, res) {
     req.query("exec insertmission @mission")
         .param('mission', req.body, TYPES.NVarChar)
         .exec(res);
+    console.log("trace1");
 });
 
 
