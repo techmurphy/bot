7781887304e02dab75384571e9ce48e4990eab03
Gaurav Dhawan <gaurav.d6@gmail.com>
2017-06-26 13:59:24 +0200

    Update setup.sql

--- setup/setup.sql
+++ setup/setup.sql
@@ -17,4 +17,19 @@ as begin
 				MissionEndDate date,
 				MissionRoster varchar(150),
 				MissionResponsibleAdviserOktaUserName varchar(200))
+	
+	insert into mission
+	select *
+	from OPENJSON (@mission,'$."CurrentMission"."OtherMissions"')
+			WITH (	MissionId varchar(150) '$.MissionId',
+				ExpertOktaUsername varchar(200) '$.ExpertOktaUsername',
+				MissionHostCountry varchar(100) '$.MissionHostCountry',
+				MissionOrganizationName varchar(15) '$.MissionOrganizationName',
+				MissionName varchar(100) '$.MissionName',
+				MissionAreaOfExpertiseCategory varchar(100) '$.MissionAreaOfExpertiseCategory',
+				MissionAreaOfExpertise varchar(150) '$.MissionAreaOfExpertise',
+				MissionStartDate date '$.MissionStartDate',
+				MissionEndDate date '$.MissionEndDate',
+				MissionRoster varchar(150) '$.MissionRoster',
+				MissionResponsibleAdviserOktaUserName varchar(200) '$.MissionResponsibleAdviserOktaUserName')
 end
