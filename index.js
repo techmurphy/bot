a41c259d8e02ecf05f09dccbe394404495eaafa3
Gaurav Dhawan <gaurav.d6@gmail.com>
2017-06-27 13:47:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use(function (req, res, next) {
     next(err);
 });
 
-/* Databse not to be used
+
 // List out all the thanks recorded in the database
 //tedious = require('tedious');
 var Connection = require('tedious').Connection;
@@ -102,7 +102,7 @@ console.log('Reading rows from the Table...');
     }
 });
 });
-*/
+
 
 // Handle the webhook subscription request from Facebook
 app.get('/webhook', function(request, response) {
