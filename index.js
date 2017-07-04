09aafcadf12c464594a983049b24d51f260ddbae
Gaurav Dhawan <gaurav.d6@gmail.com>
2017-07-04 13:27:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const verifyToken = process.env.APP_VERIFY_TOKEN;
 console.log('TRace 1');
 const app = express();
 app.use(bodyParser.json());
+console.log(req.query['hub.verify_token']);
+console.log(req.query['hub.challenge']);
 
 app.get('/webhook', (req, res) => {
     if (req.query['hub.verify_token'] === verifyToken) {
